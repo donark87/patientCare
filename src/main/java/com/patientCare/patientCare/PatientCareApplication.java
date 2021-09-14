2477f11a82c502c51edba7a06e4c2d07ede755d4
Donark <Donark87@gmail.com>
2021-09-13 21:39:13 -0400

    SpringBoot Initializr

---
+++ src/main/java/com/patientCare/patientCare/PatientCareApplication.java
@@ -0,0 +1,13 @@
+package com.patientCare.patientCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patientCare/patientCare/PatientCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patientCare.patientCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
